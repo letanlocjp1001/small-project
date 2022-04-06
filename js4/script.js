ccd96457f3522e52298c9fd05c98d27dd27d295a
Le Tan Loc <letanlocjp1001@gmail.com>
2022-04-06 22:27:49 +0900

    update

---
+++ js4/script.js
@@ -0,0 +1,44 @@
+'use strict'
+
+/*
+function calcAge(birthYear) {
+  const age = 2037 - birthYear
+  // console.log(firstName)
+
+  function printAge() {
+    let outPut = `${firstName}, you are ${age}, born is ${birthYear}`
+    console.log(outPut)
+
+    if (birthYear >= 1991 && birthYear <= 1996) {
+      var millenial = true
+      const firstName = 'Steven'
+      const str = `Oh, you are a millenial, ${firstName}`
+      console.log(str)
+
+      function add(a, b) {
+        return a + b
+      }
+
+      const outPut = 'NEW OUTPUT'
+      console.log(outPut)
+    }
+    // console.log(str)
+    console.log(millenial)
+    // console.log(add(2, 3))
+    console.log(outPut)
+  }
+  printAge()
+
+  return age
+}
+
+const firstName = 'Jonas'
+calcAge(1991)
+*/
+console.log(me)
+console.log(year)
+console.log(job)
+
+var me = 'Jonas'
+let job = 'teacher'
+const year = 1991
