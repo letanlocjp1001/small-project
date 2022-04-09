b9c675119411167777c285ca80d6bb390654866b
Le Tan Loc <letanlocjp1001@gmail.com>
2022-04-10 01:36:50 +0900

    update

--- js5/script.js
+++ js5/script.js
@@ -48,7 +48,81 @@ const restaurant = {
   },
 }
 
+const rest = new Map()
+rest.set('name', 'Classico Italiano')
+rest.set(1, 'Firenze, Italy')
+console.log(rest.set(2, 'Lisbon, Portugal'))
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are closed')
+
+console.log(rest.get('name'))
+console.log(rest.get(true))
+console.log(rest.get(1))
+
+const time = 21
+console.log(rest.get(time > rest.get('open') && rest.get('close') > time))
+
+console.log(rest.has('categories'))
+rest.delete(2)
+console.log(rest)
 /*
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+])
+console.log(ordersSet)
+console.log(new Set('LETANLOC'))
+console.log(ordersSet.size)
+console.log(ordersSet.has('Pizza'))
+console.log(ordersSet.has('Bread'))
+
+ordersSet.add('Garlic Breac')
+ordersSet.delete('Risotto')
+// ordersSet.clear()
+console.log(ordersSet)
+
+for (const order of ordersSet) {
+  console.log(order)
+}
+Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']
+const staffUnique1 = new Set(staff)
+const staffUnique2 = [...new Set(staff)]
+console.log(staffUnique1)
+console.log(staffUnique2)
+
+//Property NAMES
+const properties = Object.keys(openHours)
+console.log(properties)
+
+let openStr = `We are open on ${properties.length} day: `
+
+for (const day of properties) {
+  openStr += `${day},`
+}
+console.log(openStr)
+
+// Property VALUES
+const values = Object.values(openHours)
+console.log(values)
+
+//Entries Object
+const entries = Object.entries(openHours)
+console.log(entries)
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`)
+}
+
 ////////////////////////////////////////////////////////////////
 if (restaurant.openHours && restaurant.openHours.mon)
   console.log(restaurant.openHours.mon.open)
