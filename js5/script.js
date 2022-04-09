7a4df5059257d7111d047cb2f4ef8d2430af54f8
Le Tan Loc <letanlocjp1001@gmail.com>
2022-04-09 16:09:53 +0900

    update

--- js5/script.js
+++ js5/script.js
@@ -1,5 +1,20 @@
 'use strict'
-
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[2]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+}
+console.log(openingHours)
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30'
@@ -11,40 +26,106 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours,
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (starterMenu, mainIndex) {
+  order(starterMenu, mainIndex) {
     return [this.starterMenu[starterMenu], this.mainMenu[mainIndex]]
   },
 
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+  orderDelivery({ starterIndex, mainIndex, time, address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}will be delivered to ${address} at ${time}`
     )
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here is your declicious pasta with ${ing1}, ${ing2}, ${ing3}`)
   },
 
-  orderPizza: function (mainIngredient, ...otherIngredient) {
+  orderPizza(mainIngredient, ...otherIngredient) {
     console.log(mainIngredient)
     console.log(otherIngredient)
   },
 }
+console.log(restaurant)
+
+/*
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu]
+// console.log(menu)
+
+// for (let i = 0; i < menu.length; i++) {
+//   console.log(menu[i])
+// }
+for (const item of menu) console.log(item)
+
+for (const [index, el] of menu.entries()) {
+  console.log(`${index + 1}: ${el}`)
+}
+
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+}
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+}
+
+//OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10
+// rest2.numGuests = rest2.numGuests || 10
+// rest1.numGuests ||= 10
+// rest2.numGuests ||= 10
+
+//nullish assignment operator (null or undefined)
+rest1.numGuests ??= 10
+rest2.numGuests ??= 10
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>'
+// rest2.owner = rest2.owner && '<ANONYMOUS>'
+
+rest1.owner &&= '<ANONYMOUS>'
+rest2.owner &&= '<ANONYMOUS>'
+
+console.log(rest1)
+console.log(rest2)
+
+restaurant.numGuests = 0
+
+const guests = restaurant.numGuests || 10
+console.log(guests)
+
+//Nulish: null and undefined( NOT 0 or "")
+const guestCorrect = restaurant.numGuests ?? 10
+console.log(guestCorrect)
+
+
+console.log('---OR---')
+//Use  ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas')
+console.log('' || 'Jonas')
+console.log(true || 0)
+console.log(undefined || null)
+console.log(0 || 1)
+console.log(undefined || 0 || '' || 'Hello' || 23 || null)
+
+restaurant.numGuests = 0
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10
+console.log(guests1) //Tai numGuests khong ton tai => undefined nen no lay so 10
+
+const guests2 = restaurant.numGuests || 10
+console.log(guests2)
+
+console.log('---AND---')
+
+console.log(0 && 'JONAS')
+console.log(7 && 'JONAS')
+
+console.log('Hello' && 23 && undefined && 'jonas')
+
+
 // //SPREAD, because on RIGHT side of =
 // const arr = [1, 2, ...[3, 4]]
 // // console.log(arr)
@@ -81,7 +162,7 @@ const x = [23, 5, 7]
 add(...x)
 
 restaurant.orderPizza('mushrooms', 'onion', 'oliver', 'spinach')
-/*
+
 
 const arr = [7, 8, 9]
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]]
